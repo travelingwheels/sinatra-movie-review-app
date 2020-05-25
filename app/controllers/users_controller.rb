6f80ec379dcc18f65ffac64ec09be5196953f810
travelingwheels <gkwheeler40@gmail.com>
2020-05-24 22:21:53 -0600

    add a note about what this route does

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
   get '/login' do
     erb :'/users/login'
   end
+
+  #This route finds the user and logs them in(create a session)
+  post '/login' do
+
+  end
 end
