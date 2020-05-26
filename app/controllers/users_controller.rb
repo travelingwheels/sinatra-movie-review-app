977f852f13f198ba78f0212a65ce715b4bc91331
travelingwheels <gkwheeler40@gmail.com>
2020-05-26 02:16:09 -0600

    create route to find user, Authenticate, and log user in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 
   get '/login' do
-    erb :'/users/login'
+    erb :'users/login'
   end
 
   #This route finds the user and logs them in(create a session)
   post '/login' do
-
+    @user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/reviews/show'
+    else
+      redirect '/users/signup'
+    end
   end
 end
