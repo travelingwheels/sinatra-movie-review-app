6363f89ddb0df2ce7633e63787d871e67e6de296
travelingwheels <gkwheeler40@gmail.com>
2020-05-26 02:01:40 -0600

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    erb :index
   end
 
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @user ||= User.find_by(:id session[:user_id])
+  end
 end
