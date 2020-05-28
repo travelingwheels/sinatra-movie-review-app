cc6b70e7168bda431d7c694b46eade6b1859edc3
travelingwheels <gkwheeler40@gmail.com>
2020-05-28 08:12:49 -0600

    add post route for signup and add to post login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,21 +5,24 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-
+    if params[:name] == "" || params[:email] == "" || params[:password] == ""
+        redirect to '/signup'
+      else
+        @user = User.new(:name => params[:name], :email => params[:email], :password => params[:password])
+        @user.save
+        session[:user_id] = @user.id
+        redirect '/reviews/show'
+    end
   end
 
   get '/login' do
-    if !logged_in?
       erb :'users/login'
-    else
-      redirect to '/reviews/show'
-    end
   end
 
   #This route finds the user and logs them in(create a session)
   post '/login' do
     @user = User.find_by(email: params[:email])
-    if @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect '/reviews/show'
     else
