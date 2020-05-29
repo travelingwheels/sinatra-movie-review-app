daa30419c6a769f17d58b51a63c4f6cb69ff309d
travelingwheels <gkwheeler40@gmail.com>
2020-05-29 08:50:03 -0600

    change redirect reviews/index to just reviews, and add logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,16 @@ class UsersController < ApplicationController
         @user = User.new(:name => params[:name], :email => params[:email], :password => params[:password])
         @user.save
         session[:user_id] = @user.id
-        redirect '/reviews/show'
+        redirect '/reviews'
     end
   end
 
   get '/login' do
+     if !logged_in?
       erb :'users/login'
+     else
+      redirect '/reviews'
+    end
   end
 
   #This route finds the user and logs them in(create a session)
@@ -24,9 +28,14 @@ class UsersController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect '/reviews/show'
+      redirect '/reviews'
     else
       redirect '/users/signup'
     end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
   end
 end
