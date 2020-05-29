c527ebc9af8ea08788a6baf738f6869aed5f9afc
travelingwheels <gkwheeler40@gmail.com>
2020-05-29 08:46:33 -0600

    add get reviews route to show all reviews and get reviews new to create a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,22 @@
 class ReviewsController < ApplicationController
 
 
-  get'/reviews/show' do
-    erb :'reviews/show'
+  get'/reviews' do
+    if logged_in?
+      @reviews = Review.all
+      erb :'reviews/index'
+    end
+  end
+
+  get'/reviews/new' do
+    if logged_in?
+      erb :'reviews/new'
+    else
+      redirect '/users/login'
+    end
+  end
+
+  post'/reviews' do
+
   end
 end
