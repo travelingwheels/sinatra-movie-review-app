d820c6d4892f520b85e34a9a6a8993fd61c0cd4c
travelingwheels <gkwheeler40@gmail.com>
2020-06-04 21:07:10 -0600

    add post route logic, and reviews/id route and logic

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,15 +11,44 @@ class ReviewsController < ApplicationController
     end
   end
 
-  get'/reviews/new' do
+  get'/reviews/user_review' do
+    #if logged_in?
+      #@current_user = Review.all
+      #binding.pry
+      erb :'reviews/user_review'
+    #else
+      #redirect :'/users/login'
+    #end
+  end
+
+  get'/reviews/new' do #sends to erb form to create a new review
     if logged_in?
-      erb :'/reviews/new'
+      erb :'reviews/new'
     else
-      redirect :'users/login'
+      redirect :'/users/login'
     end
   end
 
-  post'/reviews' do
+  post'/reviews' do #this route gets info from the reviews/new page
+    if logged_in?
+      if params[:movie_name] == "" || params[:content] == "" || params[:user_id] == ""
+        redirect to "/reviews/new"
+      else
+        @review = current_user.reviews.build(movie_name: params[:movie_name], content: params[:content], user_id: params[:user_id])
+        if @review.save
+          redirect to "/reviews/#{@review.id}"
+        else
+          redirect to "/reviews/new"
+        end
+      end
+    else
+      redirect to "/users/login"
+    end
+  end
 
+  #show route for a review
+  get '/reviews/:id' do
+      @review = Review.find_by_id(params[:id])
+      erb:'reviews/show'
   end
 end
